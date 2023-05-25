6ad2e029acdf1405ce97f178566987791d8653b3
Patte <55685404+Thomasgelinflavian@users.noreply.github.com>
2023-05-25 16:12:18 +0200

    add recycler view Coins

--- app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinRecyclerAdapter.java
+++ app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinRecyclerAdapter.java
@@ -1,25 +1,39 @@
 package com.example.eseo_s8_client_server.viewmodels;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CoinRecyclerAdapter extends RecyclerView.Adapter {
+import com.example.eseo_s8_client_server.R;
+import com.example.eseo_s8_client_server.models.Coin;
+
+import java.util.List;
+
+public class CoinRecyclerAdapter extends RecyclerView.Adapter<CoinView> {
+    private final List<Coin> coins;
+
+    public CoinRecyclerAdapter(List<Coin> coins) {
+        this.coins=coins;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CoinView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_coin,
+                parent, false);
+        return new CoinView(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull CoinView holder, int position) {
+        holder.initCoin(position, coins.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.coins.size();
     }
 }
---
+++ app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinView.java
@@ -0,0 +1,21 @@
+package com.example.eseo_s8_client_server.viewmodels;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eseo_s8_client_server.models.Coin;
+
+public class CoinView extends RecyclerView.ViewHolder {
+
+    public CoinView(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void initCoin(int position, Coin coin)
+    {
+
+    }
+
+}
