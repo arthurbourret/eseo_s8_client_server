cade969405c602726941091564a1598556f206f7
Patte <55685404+Thomasgelinflavian@users.noreply.github.com>
2023-05-25 16:44:53 +0200

    Recycler finished CoinView

--- app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinView.java
+++ app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinView.java
@@ -1,10 +1,12 @@
 package com.example.eseo_s8_client_server.viewmodels;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.eseo_s8_client_server.R;
 import com.example.eseo_s8_client_server.models.Coin;
 
 public class CoinView extends RecyclerView.ViewHolder {
@@ -15,7 +17,7 @@ public class CoinView extends RecyclerView.ViewHolder {
 
     public void initCoin(int position, Coin coin)
     {
-
+        ((TextView) itemView.findViewById(R.id.name_coin)).setText(coin.getName());
     }
 
 }
