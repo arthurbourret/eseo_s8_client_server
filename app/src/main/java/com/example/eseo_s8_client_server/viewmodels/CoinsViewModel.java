5f8622d995e47edc8bf26a42c6c3a9d8626eab62
Patte <55685404+Thomasgelinflavian@users.noreply.github.com>
2023-06-08 15:35:51 +0200

    icons picasso

--- app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinsViewModel.java
+++ app/src/main/java/com/example/eseo_s8_client_server/viewmodels/CoinsViewModel.java
@@ -1,6 +1,9 @@
 package com.example.eseo_s8_client_server.viewmodels;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
@@ -12,13 +15,27 @@ import com.example.eseo_s8_client_server.models.CoinsResponse;
 import com.example.eseo_s8_client_server.network.NetworkCallBack;
 import com.example.eseo_s8_client_server.network.RetrofitNetworkManager;
 import com.example.eseo_s8_client_server.storage.DataRepository;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 public class CoinsViewModel extends AndroidViewModel implements IViewModel<List<Coin>> {
+    private final OkHttpClient client = new OkHttpClient();
+
     private final MutableLiveData<List<Coin>> data;
+    private final MutableLiveData<Map<String, Bitmap>> icons;
     private final DataRepository dataRepository;
     private final CoinsData coins;
 
@@ -27,12 +44,17 @@ public class CoinsViewModel extends AndroidViewModel implements IViewModel<List<
         this.dataRepository = new DataRepository(application);
         this.data = new MutableLiveData<>();
         this.dataRepository.getData().observeForever(data::postValue);
+        this.icons = new MutableLiveData<>();
 
         this.coins = new CoinsData();
     }
 
     public LiveData<List<Coin>> getData() {
         return data;
+    }
+
+    public LiveData<Map<String, Bitmap>> getIcons() {
+        return icons;
     }
 
     // TODO: pourquoi le varargs en paramètre ?
@@ -55,8 +77,31 @@ public class CoinsViewModel extends AndroidViewModel implements IViewModel<List<
         data.postValue(coins.getCoins());
     }
 
+    private void fetchIcons(List<Coin> coins) {
+        for (Coin coin : coins) {
+            String url = coin.getIconUrl();
+            url.replace(".svg", ".png");
+            fetchIcon(url, coin.getUuid());
+        }
+    }
+
+    private void fetchIcon(String url, String uuid) {
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new NetworkCallBack.OkHttpCallback() {
+            @Override
+            public void onSuccess(ResponseBody response) {
+                Bitmap icon = getIconFromStream(response.byteStream());
+                Map<String, Bitmap> tmp = icons.getValue();
+                if (tmp == null) tmp = new HashMap<>();
+                tmp.put(uuid, icon);
+                icons.postValue(tmp);
+            }
+        });
+    }
+
     private void handleResponse(CoinsResponse response) {
         this.coins.setCoinList(response.getData());
+        this.fetchIcons(coins.getCoins());
         saveCoins(coins.getCoins());
     }
 
@@ -64,5 +109,11 @@ public class CoinsViewModel extends AndroidViewModel implements IViewModel<List<
         for (Coin coin : coins) {
             this.dataRepository.insertData(coin);
         }
+    }
+
+    private Bitmap getIconFromStream(InputStream stream) {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(stream);
+
+        return BitmapFactory.decodeStream(bufferedInputStream);
     }
 }
